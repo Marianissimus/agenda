421dc483693476fdd3058626c7f26a81b0bbcb7a
Marianissimus <marianissimus@gmail.com>
2023-07-12 07:58:11 +0300

    update deploy.yml

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
-publicPath: 'agenda'
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/agenda/" : "/",
+})
